e434558355482b6e553748aa7de4b8c237e2227a
alex-toy <alexei.80@hotmail.fr>
2024-08-10 06:39:03 +0200

    init

---
+++ GestionImmoApp/GestionImmo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestionImmo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
