91bac8e5f04b24926b41e22402cf5cfd26c35257
alex-toy <alexei.80@hotmail.fr>
2024-08-10 08:34:16 +0200

    site crud

---
+++ GestionImmoApp/GestionImmo/Controllers/SiteController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GestionImmo.Entities;
+using GestionImmo.Repo;
+
+namespace GestionImmo.Controllers
+{
+    public class SiteController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public SiteController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Site
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.SiteDb.ToListAsync());
+        }
+
+        // GET: Site/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var site = await _context.SiteDb
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (site == null)
+            {
+                return NotFound();
+            }
+
+            return View(site);
+        }
+
+        // GET: Site/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Site/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Code,Name,Address")] Site site)
+        {
+            if (ModelState.IsValid)
+            {
+                site.Id = Guid.NewGuid();
+                _context.Add(site);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(site);
+        }
+
+        // GET: Site/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var site = await _context.SiteDb.FindAsync(id);
+            if (site == null)
+            {
+                return NotFound();
+            }
+            return View(site);
+        }
+
+        // POST: Site/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Code,Name,Address")] Site site)
+        {
+            if (id != site.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(site);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SiteExists(site.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(site);
+        }
+
+        // GET: Site/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var site = await _context.SiteDb
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (site == null)
+            {
+                return NotFound();
+            }
+
+            return View(site);
+        }
+
+        // POST: Site/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var site = await _context.SiteDb.FindAsync(id);
+            if (site != null)
+            {
+                _context.SiteDb.Remove(site);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SiteExists(Guid id)
+        {
+            return _context.SiteDb.Any(e => e.Id == id);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# GestionImmo : application de gestion du parc immobilier à destination des mairies et collectivités
+# GestionImmo - Application de gestion du parc immobilier à destination des mairies et collectivités
 
 ## Packages
 ```
 Microsoft.EntityFrameworkCore.SqlServer
 Microsoft.EntityFrameworkCore.Tools
+```
+
+## Migrations
+```
+add-migration "batiment et site"
+update-database
 ```
